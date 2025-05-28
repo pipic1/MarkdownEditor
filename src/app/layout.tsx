1af98c45ea8c7d7953e4f3880678a5425d4309b3
Pierre Pic <pierre.pic63122@gmail.com>
2025-05-28 14:32:50 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" sizes="192x192" href="/window.svg" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
